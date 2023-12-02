14fc0494b5669b0154fc2e042efd93fdcd94b47e
unknown <studzinskis46@gmail.com>
2023-12-02 19:02:56 +0100

    feat: tests- RegisterModal -integration - error handling test/ fix:correct api enpoint test instead of mock

--- __tests__/(integrationTests)/modals/RegisterModal.test.tsx
+++ __tests__/(integrationTests)/modals/RegisterModal.test.tsx
@@ -7,13 +7,15 @@ import RegisterModal from '@/components/modals/RegisterModal';
 import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
+import axios from 'axios'
 
 // Mock next/router
 jest.mock('next/navigation', () => ({
     useRouter: jest.fn(),
   }));
 
-  jest.mock('react-hot-toast');
+jest.mock('react-hot-toast');
+jest.mock('axios');
   
 const useRouterMock = useRouter as jest.Mock;
 const pushMock = jest.fn();
@@ -60,10 +62,12 @@ jest.mock('next-auth/react', () => ({
 
 describe('RegisterModal component', () => {
   it('renders correctly and submits form', async () => {
-
+    // Mocking that the user is not authenticated
     (useRouterMock as jest.Mock).mockReturnValue({});
-    (signIn as jest.Mock).mockReturnValueOnce({});
     (useSession as jest.Mock).mockReturnValueOnce([null, false, undefined]);
+
+    // Mock a successful registration
+    (axios.post as jest.Mock).mockResolvedValueOnce({});
 
     render(<RegisterModal />);
 
@@ -80,9 +84,16 @@ describe('RegisterModal component', () => {
 
     // Wait for asynchronous operations to complete
     await waitFor(() => {
-      // You can add assertions here based on the behavior of your component
-      expect(useLoginModalMock).toHaveBeenCalled();
-      // For example, check if the useRegisterModal.onClose function is calle
+      // Assert that axios.post function is called
+      expect(axios.post).toHaveBeenCalledWith(`/api/register`, {
+        name: 'Sime',
+        email: 'sime@gmail.com',
+        password: 'sime1234',
+      });
+
+      // You can add more assertions here based on the behavior of your component
+      expect(useRegisterModalMock().onClose).toHaveBeenCalled();
+      expect(toast.success).toHaveBeenCalledWith('Registered successfully');
     });
   });
 
@@ -102,6 +113,43 @@ describe('RegisterModal component', () => {
       // You can add assertions here based on the behavior of your component
       // For example, check if the signIn function is called with 'google'
       expect(signIn).toHaveBeenCalledWith('google');
+    });
+  });
+
+  it('handles error response during form submission', async () => {
+    // Mocking that the user is not authenticated
+    (useRouterMock as jest.Mock).mockReturnValue({});
+    (useSession as jest.Mock).mockReturnValueOnce([null, false, undefined]);
+
+    // Mock a failed registration
+    (axios.post as jest.Mock).mockRejectedValueOnce({});
+
+    render(<RegisterModal />);
+
+    // Check if the component renders
+    expect(screen.getByTestId('register-form')).toBeInTheDocument();
+
+    // Mock user entering email and password
+    await userEvent.type(screen.getByTestId('name-field'), 'Sime');
+    await userEvent.type(screen.getByTestId('email-field'), 'simon@gmail.com');
+    await userEvent.type(screen.getByTestId('password-field'), 'simon1234');
+
+    // Mock form submission
+    fireEvent.click(screen.getByTestId('register-btn'));
+
+    // Wait for asynchronous operations to complete
+    await waitFor(() => {
+      // Assert that axios.post function is called
+      expect(axios.post).toHaveBeenCalledWith(`/api/register`, {
+        email: 'simon@gmail.com',
+        password: 'simon1234',
+        name: 'Sime'
+      });
+    });
+
+    // Wait for the error message to be displayed
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalledWith('Something went wrong');
     });
   });
 });
