c1c68b620bf39dfa2bb6e9b7877a4c98fec0c2ec
unknown <studzinskis46@gmail.com>
2023-12-10 14:12:50 +0100

    Husky tests -1

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,35 +1,31 @@
+import Container from "@/components/Container";
+import { PageWrapper } from "@/components/animations/pageWrapper";
+import AdvertBlock from "@/components/hero/AdvertBlock";
+import Footer from "@/components/hero/Footer";
+import HeroAction from "@/components/hero/HeroAction";
+import HeroServices from "@/components/hero/HeroServices";
+import HeroTop from "@/components/hero/HeroTop";
+import Herobanner from "@/components/hero/Herobanner";
+import Testimonials from "@/components/hero/Testimonials";
 
-import getCurrentUser from '@/actions/get-current-user'
-import Container from '@/components/Container'
-import { PageWrapper } from '@/components/animations/pageWrapper'
-import AdvertBlock from '@/components/hero/AdvertBlock'
-import Footer from '@/components/hero/Footer'
-import HeroAction from '@/components/hero/HeroAction'
-import HeroServices from '@/components/hero/HeroServices'
-import HeroTop from '@/components/hero/HeroTop'
-import Herobanner from '@/components/hero/Herobanner'
-import Testimonials from '@/components/hero/Testimonials'
-
-
-export const dynamic = 'force-dynamic' 
+export const dynamic = "force-dynamic";
 
 export default async function Landing() {
-
   return (
-  <Container>
-    <PageWrapper>
-    <div className='pt-32'>
-      <div className='h-1/3 pb-10'>
-        <HeroTop />
-      </div>
-      <HeroAction />
-      <Herobanner />
-      <HeroServices />
-      <Testimonials />
-      <AdvertBlock />
-      <Footer />
-    </div>
-    </PageWrapper>
-  </Container>
-  )
+    <Container>
+      <PageWrapper>
+        <div className="pt-32">
+          <div className="h-1/3 pb-10">
+            <HeroTop />
+          </div>
+          <HeroAction />
+          <Herobanner />
+          <HeroServices />
+          <Testimonials />
+          <AdvertBlock />
+          <Footer />
+        </div>
+      </PageWrapper>
+    </Container>
+  );
 }
