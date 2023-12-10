8ec26c29391cd8c58e02b3e88c3a892f523c791c
unknown <studzinskis46@gmail.com>
2023-12-10 14:27:22 +0100

    fix: tests - Notification Card hour

--- __tests__/(unitTests)/NotificationCard.test.tsx
+++ __tests__/(unitTests)/NotificationCard.test.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import NotificationCard from '@/components/NotificationCard';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import NotificationCard from "@/components/NotificationCard";
 
-describe('NotificationCard component', () => {
+describe("NotificationCard component", () => {
   const notificationData = {
-    body: 'Test notification body',
-    userId: 'testUserId',
-    createdAt: '2023-11-23T12:34:56Z',
-    userName: 'Test User',
+    body: "Test notification body",
+    userId: "testUserId",
+    createdAt: "2023-11-23T12:34:56Z",
+    userName: "Test User",
   };
 
-  it('renders notification card correctly', () => {
+  it("renders notification card correctly", () => {
     render(
       <NotificationCard
         body={notificationData.body}
@@ -21,13 +21,20 @@ describe('NotificationCard component', () => {
     );
 
     // Assert that the component renders the notification content
-    expect(screen.getByText(new RegExp(notificationData.body))).toBeInTheDocument();
-    expect(screen.getByText(/13:34/)).toBeInTheDocument(); // Adjust the pattern as needed
-    expect(screen.getByText(new RegExp(notificationData.userName))).toBeInTheDocument();
+    expect(
+      screen.getByText(new RegExp(notificationData.body))
+    ).toBeInTheDocument();
+    expect(screen.getByText(/12:34/)).toBeInTheDocument(); // Adjust the pattern as needed
+    expect(
+      screen.getByText(new RegExp(notificationData.userName))
+    ).toBeInTheDocument();
 
     // Optionally, you can assert that the image is rendered
-    const image = screen.getByAltText('');
+    const image = screen.getByAltText("");
     expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute('src',"/_next/image?url=%2Fimages%2Fpexels-photo-2608582.jpeg&w=256&q=75");
+    expect(image).toHaveAttribute(
+      "src",
+      "/_next/image?url=%2Fimages%2Fpexels-photo-2608582.jpeg&w=256&q=75"
+    );
   });
 });
