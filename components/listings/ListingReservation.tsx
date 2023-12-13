48130fb243eb4595aa5f0679227a1c1966e8aaeb
unknown <studzinskis46@gmail.com>
2023-12-13 12:40:08 +0100

    fix: Listing Reservation hours text more visible

--- components/listings/ListingReservation.tsx
+++ components/listings/ListingReservation.tsx
@@ -1,32 +1,40 @@
-'use client'
+"use client";
 
-import { Datetype } from "@/app/(routes)/(listing)/listings/[listingId]/components/ListingSingle"
-import Calendar from "../Calendar"
-import { Button } from "../ui/button"
-import { Dispatch, SetStateAction } from "react"
-
+import { Datetype } from "@/app/(routes)/(listing)/listings/[listingId]/components/ListingSingle";
+import Calendar from "../Calendar";
+import { Button } from "../ui/button";
+import { Dispatch, SetStateAction } from "react";
 
 interface ListingReservationProps {
-  onSubmit: () => void
-  date: Datetype
-  setDate: Dispatch<SetStateAction<Datetype>>
-  dDates: Date[]
+  onSubmit: () => void;
+  date: Datetype;
+  setDate: Dispatch<SetStateAction<Datetype>>;
+  dDates: Date[];
 }
 
-const ListingReservation = ({onSubmit, setDate, date ,dDates}:
-   ListingReservationProps) => {
+const ListingReservation = ({
+  onSubmit,
+  setDate,
+  date,
+  dDates,
+}: ListingReservationProps) => {
   return (
     <div className="">
-      <p className="font-italic text-xl px-5 text-center">Choose day and hour</p>
-      <Calendar setDate={setDate}  date={date} disabledDates={dDates}/>
+      <p className="font-italic font-semibold text-xl px-5 text-center">
+        Choose day and hour
+      </p>
+      <Calendar setDate={setDate} date={date} disabledDates={dDates} />
       <div className="pt-4 w-full flex justify-center items-center gap-x-3">
-          <Button
-          data-testid="time-button-0" 
-            onClick={onSubmit}
-            className="w-full mx-20">Reserve</Button>
+        <Button
+          data-testid="time-button-0"
+          onClick={onSubmit}
+          className="w-full mx-20"
+        >
+          Reserve
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListingReservation
+export default ListingReservation;
